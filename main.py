21f2db1d7a3e44b45bf191820fb82a69ff0d64ea
Shaker Mahmud Khandaker <shaker@khandakerrahin.com>
2021-12-18 05:19:29 +0100

    JSON read, write

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import json
 
+def getCondition(name):
+    print(name)
+    return name
+
+# read JSON file
+with open('patients_sample.json') as f:
+    data = json.load(f)
+
+# loop through JSON file
+for patient in data:
+    # print(patient['name'])
+
+    patient['condition'] = getCondition('Yanni')
+    patient['trials'] = getCondition('Hans Zimmer')
+
+# dump new JSON file
+with open('patients.json', 'w') as f:
+    json.dump(data, f, indent=4)
